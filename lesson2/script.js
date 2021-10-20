4a3b81f66d1472214642fe32a8980b6ea96f82cc
Leandro Gehlen <91934483+scubaDevs@users.noreply.github.com>
2021-10-20 13:03:27 -0300

    update

---
+++ lesson2/script.js
@@ -0,0 +1,10 @@
+// DOM = DOCUMENT OBJET MODEL
+/*
+
+THAT means that every html element on the document is treated as an object and can be manipulated by javascrip commands.
+
+tHAT WAY, YOU CAN USE JAVASCRIP TO CHANGE AND MANIPULATE EVERYTHING ON THE DOCUMENT
+
+
+*/
+
